5ab3139b7e5585d02487506da1540ec60c69f08a
CedricBorko <cedric@borkoland.de>
2021-11-30 14:44:44 +0100

    Icons/img

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import sys
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon, QAction, QDesktopServices
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 
 from color_window import ColorWindow
 
 
 def main():
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+
+    icon = QIcon("icons/tray_icon.png")
+
+    tray = QSystemTrayIcon()
+    tray.setIcon(icon)
+    tray.setVisible(True)
+
+    menu = QMenu()
     window = ColorWindow()
-    window.show()
+
+    close_action = QAction("Schließen")
+    close_action.triggered.connect(app.quit)
+
+    open_action = QAction("Fenster öffnen")
+    open_action.triggered.connect(window.show)
+
+    google_color_picker_action = QAction("Color Picker Google")
+    google_color_picker_action.triggered.connect(
+        QDesktopServices.openUrl(
+            "https://imagecolorpicker.com/color-code/0051ff"
+        )
+    )
+
+    menu.addAction(close_action)
+    menu.addAction(open_action)
+    menu.addAction(google_color_picker_action)
+
+    tray.setContextMenu(menu)
     sys.exit((app.exec_()))
 
 
